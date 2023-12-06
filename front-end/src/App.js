ca97dfa220fca75487a6d081a58ba6b53a12e4e6
Akash <akashathnure40@gmail.com>
2023-12-06 22:10:19 +0530

    header footer added in ui

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-   
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<h1>Product</h1>} />
+        <Route path='/add' element={<h1>Product</h1>} />
+        <Route path='/update' element={<h1>Product</h1>} />
+        <Route path='/logout' element={<h1>Product</h1>} />
+        <Route path='/profile' element={<h1>Product</h1>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
