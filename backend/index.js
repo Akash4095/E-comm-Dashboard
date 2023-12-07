281bb4bb69850a2076b08f6eb5e2534cfd5cc3ee
Akash <akashathnure40@gmail.com>
2023-12-07 10:27:13 +0530

    signup api and page done

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+require("./db/config")
+const User = require('./db/user')
 const app = express()
+
+
+app.use(express.json())
+
+app.post("/signup", async (req, res) => {
+    let user = new User(req.body)
+    let result = await user.save()
+    res.send(result)
+})
 
 app.get("/", (req, res) => {
     res.send('app working')
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+import Signup from './components/signup';
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
         <Route path='/update' element={<h1>Product</h1>} />
         <Route path='/logout' element={<h1>Product</h1>} />
         <Route path='/profile' element={<h1>Product</h1>} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
